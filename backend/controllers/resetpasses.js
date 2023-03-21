cacf670ccf9f6ec38004adbc4eac8749c084bd97
spyrgkou <spyridng@gmail.com>
2023-03-21 19:40:51 +0200

    Clear Code - useless comments

--- backend/controllers/resetpasses.js
+++ backend/controllers/resetpasses.js
@@ -20,13 +20,11 @@ module.exports = async (req, res, next) => {
         
         var arrayToInsert = [];
         var passFilename =__dirname+'/../static/sampledata01_passes100_8000.csv';
-        // var passFilename =__dirname+'/../static/days/day0.csv';
         await csvtojson().fromFile(passFilename).then(csvdata => {
             for (var i=0; i<csvdata.length; i++){
                 var oneRow = {
                     passId: csvdata[i]["passID"],
                     timeStamp: inputCSVDateCorrection(csvdata[i]["timestamp"]),
-                    // timeStamp: csvdata[i]["timestamp"],
                     stationRef: csvdata[i]["stationRef"],
                     vehicleRef: csvdata[i]["vehicleRef"],
                     charge: csvdata[i]["charge"]
@@ -35,37 +33,14 @@ module.exports = async (req, res, next) => {
             }
         });
 
-
         Pass.insertMany(arrayToInsert, (err, result)=>{
             if (err) res.status(500).json({"Status":err.message});
             if (result) {
                 res.status(200).json({"Status":"OK"});
             }
         });
-        // for (var j=0; j<1; j++){
-        //     var passFilename =__dirname+'/../static/days/day'+j.toString()+'.csv';
-        //     await csvtojson().fromFile(passFilename).then(csvdata => {
-        //         for (var i=0; i<csvdata.length; i++){
-        //             var oneRow = {
-        //                 passId: csvdata[i]["passID"],
-        //                 timeStamp: ISODateFromCSV(csvdata[i]["timestamp"]),
-        //                 stationRef: csvdata[i]["stationRef"],
-        //                 vehicleRef: csvdata[i]["vehicleRef"],
-        //                 charge: csvdata[i]["charge"]
-        //             };
-        //             arrayToInsert.push(oneRow);
-        //         }
-        //     });
-        //     Pass.insertMany(arrayToInsert, (err, result)=>{
-        //         if (err) res.status(500).json({"Status":err.message});
-        //         // if (result) {
-        //         //     res.status(200).json({"Status":"OK"});
-        //         // }
-        //     });
-        // }
-        // res.status(200).json({"Status":"OK"});
+
     } catch (error) {
-        // res.status(500).json({"Status":"Something went wrong!"});
         next(new ExpressError(error.message, 500));
 	}
 };
